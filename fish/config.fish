f6a4187e379b93858dd6c6e7c48e4edf6818a78a
vercas <contact@vercas.com>
2016-08-19 15:43:02 +0300

    Added hash-based hostname selection

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,8 @@ set -g theme_show_exit_status yes
 set -g theme_title_display_process yes
 set -g theme_display_ruby no
 
-switch (hostname)
-case VULT
+switch (echo -n (hostname) | shasum | head -c 40)
+case 21d705be9a22b5b5ac7d64c1de6aa820d168a457
 	set -g theme_color_scheme base16-light
 end
 
