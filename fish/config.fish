360c2d2edd5141fe371851ddc8a6224074a2af64
vercas <contact@vercas.com>
2016-08-19 15:26:09 +0300

    Updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,11 @@
-set -g theme_color_scheme base16-light
 set -g theme_display_vi yes
 set -g theme_display_git yes
+set -g theme_show_exit_status yes
+set -g theme_title_display_process yes
+set -g theme_display_ruby no
+
+switch (hostname)
+case VULT
+	set -g theme_color_scheme base16-light
+end
 
