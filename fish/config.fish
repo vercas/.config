bf3c3c1e5d2120436955c98df8fcc5525f6774db
vercas <contact@vercas.com>
2016-08-20 20:40:59 +0100

    Changed 'shasum' to 'sha1sum' for portability

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ set -g theme_show_exit_status yes
 set -g theme_title_display_process yes
 set -g theme_display_ruby no
 
-switch (echo -n (hostname) | shasum | head -c 40)
+switch (echo -n (hostname) | sha1sum | head -c 40)
 case 21d705be9a22b5b5ac7d64c1de6aa820d168a457
 	set -g theme_color_scheme base16-light
 case 27d30547559b399e16c744df4c2bcbd41974851b
