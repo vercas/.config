3e7f4975dcf90b0663b9f30a7d3b128c665c138b
vercas <contact@vercas.com>
2016-08-20 17:39:47 +0100

    Added new host to Fish config

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,7 @@ set -g theme_display_ruby no
 switch (echo -n (hostname) | shasum | head -c 40)
 case 21d705be9a22b5b5ac7d64c1de6aa820d168a457
 	set -g theme_color_scheme base16-light
+case 27d30547559b399e16c744df4c2bcbd41974851b
+	set -g theme_color_scheme terminal-dark
 end
 
