36abf4050f11f26179648d855347009d6f4eee89
vercas <contact@vercas.com>
2016-08-21 17:20:41 +0100

    Fixed 'wat' function

--- fish/functions/wat.fish
+++ fish/functions/wat.fish
@@ -2,7 +2,14 @@ function wat -d "Runs 'll' or 'less', depending on which is the right one."
     if test (count $argv) = 0
         ll
     else
-        less $argv 2> /dev/null; or ll $argv
+        if test -d $argv[1]
+            ll $argv
+        else if test -f $argv[1]
+            less $argv
+        else
+            echo "wat: '$argv[1]' is not a file nor a directory" >&2
+            return 50
+        end
     end
 end
 
