40258981d45baf4e89606628d1709fa09174bea0
Vercas <contact@vercas.com>
2017-04-09 01:59:59 +0100

    Updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
 set -g theme_display_vi yes
 set -g theme_display_git yes
 set -g theme_show_exit_status yes
@@ -14,5 +21,11 @@ case 21d705be9a22b5b5ac7d64c1de6aa820d168a457
     end
 case 27d30547559b399e16c744df4c2bcbd41974851b
     set -g theme_color_scheme terminal-dark
+case 7e7f4d3b88269bca7efea6939d35ae537028f864
+    set -gx GOPATH "$HOME/go"
+end
+
+if not contains ~/bin $PATH
+    set -gx PATH ~/bin $PATH
 end
 
