bec6397bf90e907c7bee873b2640b4f6ae5a9f46
Vercas <contact@vercas.com>
2017-04-10 21:44:03 +0100

    Updated fish greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,26 +1,35 @@
 function fish_greeting
-    if type -qf fortune
-        if type -qf cowsay
-            set -l twid (math (tput cols) - 3)
+    set -l cows cowsay
 
-            if test $status -ne 0
-                # Failed to get columns, meh...
-                fortune -as | cowsay -s
-            else
-                fortune -as | cowsay -W $twid -s
-            end
-        else
-            fortune -as
-        end
-    else
-        # This really is no fun. :L
-        # I dunno what to do instead.
+    if not type -qf $cows
+        set cows '/usr/games/cowsay'
     end
 
-    #set -l packageNotifier "/usr/lib/update-notifier/apt-check"
+    if type -qf $cows
+        set -l twid (math (tput cols) - 3)
 
-    #if test -x $packageNotifier
-    #eval $packageNotifier --human-readable
-    #end
+        if test $status -ne 0
+            # Failed to get columns, meh...
+            set cows '|' $cows -s
+        else
+            set cows '|' $cows -W $twid -s
+        end
+    else
+        set cows ''
+    end
+
+    set -l fort fortune
+
+    if not type -qf $fort
+        set fort '/usr/games/fortune'
+    end
+
+    set -l aptc '/usr/lib/update-notifier/apt-check'
+
+    if type -qf $fort
+        eval $fort -as $cows
+    else if test -x $aptc
+        eval $aptc --human-readable $cows
+    end
 end
 
