5f69069a43e548ac902b98cd273946ae5326d3a4
vercas <contact@vercas.com>
2017-04-23 22:58:38 +0100

    Updated prepare-tmux.sh

--- prepare-tmux.sh
+++ prepare-tmux.sh
@@ -3,21 +3,6 @@
 # Some goodies.
 source $HOME/.config/shell-utilities.sh
 
-lua -v > /dev/null || exit 1
-
-LPP=$(lua -e "print(package.path)")
-LPP_REGEX='([^;]*\/usr(\/local)?\/share\/lua\/5\.[0-9]+)\/\?.lua\b'
-
-if [[ $LPP =~ $LPP_REGEX ]]; then
-    LDIR="${BASH_REMATCH[1]}"
-else
-    echo "Failed to find a useable Lua include directory in:" $LPP 1>&2
-    echo "Regex:" "${LPP_REGEX}"
-    exit 2
-fi
-
-sudo ln -s $CDIR/vline.lua $LDIR/vline.lua
-
 if [[ -n $HAS_HBDIR ]]; then
     # So the home bin directory was already in the path.
 
