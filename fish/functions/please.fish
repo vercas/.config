026be229c78c5ae7b31ba0c47cfe2ae8bed9c8db
vercas <contact@vercas.com>
2019-01-06 12:45:32 +0000

    Updated 'please' function

--- fish/functions/please.fish
+++ fish/functions/please.fish
@@ -1,4 +1,22 @@
 function please -d "Retry the previous command with 'sudo'."
-    eval sudo $history[1]
+    if test $status -ne 0
+        set -l cmd $history[1]
+
+        if test -z $argv[1]
+            set -l index 1
+
+            while test $cmd = (status current-function)
+                set index (math $index + 1)
+                set cmd $history[$index]
+            end
+        else
+            set cmd $history[$argv[1]]
+        end
+
+        eval sudo $cmd
+    else
+        echo "Previous function did not fail." 1>&2
+        return 1
+    end
 end
 
