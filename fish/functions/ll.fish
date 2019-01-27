542b1e47f16ee0d9f0bd8d3b149d5ab38e42a3a8
Vercas <contact@vercas.com>
2019-01-27 14:05:24 +0000

    Updated ll macro to use automatic paging

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,6 +1,6 @@
 function ll -w ls
     if which lsd > /dev/null
-        lsd -laF $argv
+        lsd -laF $argv --color always --icon always | less -RFX
     else
         ls -lAhF --color=auto $argv
     end
