64fff223ef2a4c6b0e60d4250f482c266338f588
Vercas <contact@vercas.com>
2020-01-01 18:23:50 +0000

    Update mkcd function to do mkdir -p

--- fish/functions/mkcd.fish
+++ fish/functions/mkcd.fish
@@ -1,7 +1,7 @@
 function mkcd -d "Creates a directory and changes to it."
     if test (count $argv) = 1
         if test ! -e $argv[1]
-            mkdir $argv[1]; and cd $argv[1]
+            mkdir -p $argv[1]; and cd $argv[1]
         else
             echo "mkcd: Cannot create directory '$argv[1]': File exists" >&2
             return 50
