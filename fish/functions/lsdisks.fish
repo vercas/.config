90177fd708b4aabc69eec192f31aa112d0211ab6
Vercas <contact@vercas.com>
2020-09-21 12:40:06 +0100

    Add grc coloring to lsdisks when available

--- fish/functions/lsdisks.fish
+++ fish/functions/lsdisks.fish
@@ -1,4 +1,8 @@
 function lsdisks
-    lsblk -o NAME,SIZE,HOTPLUG,RO,TYPE,FSTYPE,MOUNTPOINT | grep -v loop
+    if which grc > /dev/null
+        grc lsblk -o NAME,SIZE,HOTPLUG,RO,TYPE,FSTYPE,MOUNTPOINT | grep -v loop
+    else
+        lsblk -o NAME,SIZE,HOTPLUG,RO,TYPE,FSTYPE,MOUNTPOINT | grep -v loop
+    end
 end
 
