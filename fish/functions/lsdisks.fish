792ab457f32d03a6d8696eccd87ec78359355367
Vercas <contact@vercas.com>
2022-02-17 19:47:34 +0000

    Add color to lsdisks

--- fish/functions/lsdisks.fish
+++ fish/functions/lsdisks.fish
@@ -1,6 +1,6 @@
 function lsdisks
     if which grc > /dev/null
-        grc lsblk -o NAME,SIZE,HOTPLUG,RO,TYPE,FSTYPE,MOUNTPOINT | grep -v loop
+        grc --colour=on lsblk -o NAME,SIZE,HOTPLUG,RO,TYPE,FSTYPE,MOUNTPOINT | grep -v loop
     else
         lsblk -o NAME,SIZE,HOTPLUG,RO,TYPE,FSTYPE,MOUNTPOINT | grep -v loop
     end
