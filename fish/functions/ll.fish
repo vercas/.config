c5a6cb77e04e2c06ddef1299b13d0aed2ecec5f4
Vercas <contact@vercas.com>
2023-10-01 19:56:07 +0100

    Change file modification time format in 'll' Fish function

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,6 +1,6 @@
 function ll -w ls
     if type -q lsd
-        lsd -laF $argv --color always --icon always | less -RFX
+        lsd -laF --color always --icon always --git --date '+%y-%m-%d %H-%M-%S' $argv | less -RFX
     else
         ls -lAhF --color=auto $argv
     end
